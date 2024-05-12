a1edb8ac3f6b24e7ad7b6e1f1d870372e559a4b9
julian-wasmeier-titanom <julian.wasmeier@titanom.com>
2024-05-12 13:07:12 +0200

    feat: change jwt expiration

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { LocalStrategy } from './local.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '2592000s' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
