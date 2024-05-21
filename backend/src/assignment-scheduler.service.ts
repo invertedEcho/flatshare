c95d3993cdf82ee7974ae386b91003eaab7f2b36
julian-wasmeier-titanom <julian.wasmeier@titanom.com>
2024-05-21 19:50:48 +0200

    feat: scheduling logic

--- backend/src/assignment-scheduler.service.ts
+++ backend/src/assignment-scheduler.service.ts
@@ -1,63 +1,93 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import {
-  dbGetTaskGroupUsers,
-  dbGetTasksOfTaskGroup,
-} from './db/functions/task-group';
-import {
-  dbCreateTaskGroupAssignment,
-  dbGetAssignmentsForTaskGroup,
-  dbGetTaskGroupsToAssignForCurrentInterval,
-} from './db/functions/assignment-task-group';
-import { randomFromArray } from './utils/array';
+import { eq, sql } from 'drizzle-orm';
 import { db } from './db';
-import { assignmentTable } from './db/schema';
+import {
+  dbGetAssignmentsForTaskGroup,
+  dbGetTasksToAssignForCurrentInterval,
+} from './db/functions/assignment';
+import { dbGetTaskGroupUsers } from './db/functions/task-group';
+import { assignmentTable, taskGroupTable, taskTable } from './db/schema';
+import { randomFromArray } from './utils/array';
 
 @Injectable()
 export class AssignmentSchedulerService {
-  @Cron(CronExpression.EVERY_5_SECONDS)
+  @Cron(CronExpression.EVERY_10_SECONDS)
   async handleCron() {
-    console.debug('CRON job running');
-    const taskGroupsToCreateAssignmentsFor =
-      await dbGetTaskGroupsToAssignForCurrentInterval();
-    console.debug({ taskGroupsToCreateAssignmentsFor });
-    for (const { taskGroupId } of taskGroupsToCreateAssignmentsFor) {
-      const userIds = await dbGetTaskGroupUsers(taskGroupId);
+    const tasksToCreateAssignmentsFor =
+      await dbGetTasksToAssignForCurrentInterval();
+
+    console.debug(
+      'Running task scheduling cron job for',
+      tasksToCreateAssignmentsFor,
+    );
+
+    const tasksByGroup = tasksToCreateAssignmentsFor.reduce<
+      Record<string, number[]>
+    >((acc, curr) => {
+      if (!acc[curr.taskGroupId]) {
+        acc[curr.taskGroupId] = [];
+      }
+      acc[curr.taskGroupId].push(curr.taskId);
+      return acc;
+    }, {});
+
+    for (const [taskGroupId, taskIds] of Object.entries(tasksByGroup)) {
+      // FIXME: ugly type conversion, maybe use a map
+      const userIds = await dbGetTaskGroupUsers(Number(taskGroupId));
       if (userIds.length === 0) {
         continue;
       }
-      const lastAssignments = await dbGetAssignmentsForTaskGroup(
-        taskGroupId,
-        userIds.length,
-      );
-      const firstTimeAssignmentUsers = userIds.filter(
-        ({ userId }) =>
-          !lastAssignments.some((assignment) => assignment.userId === userId),
+
+      const nextResponsibleUserId = await getNextResponsibleUserId(
+        Number(taskGroupId),
+        userIds.map(({ userId }) => userId),
       );
 
-      console.debug({ userIds });
-      console.debug({ firstTimeAssignmentUsers });
-      console.debug({ lastAssignments, taskGroupId });
-
-      let nextResponsibleUserId: number;
-      if (firstTimeAssignmentUsers.length === 0) {
-        nextResponsibleUserId =
-          lastAssignments[lastAssignments.length - 1].userId;
-      } else {
-        nextResponsibleUserId = randomFromArray(
-          firstTimeAssignmentUsers,
-        ).userId;
-      }
-
-      await dbCreateTaskGroupAssignment(taskGroupId, nextResponsibleUserId);
-      const tasksOfTaskGroup = await dbGetTasksOfTaskGroup(taskGroupId);
-      const assignmentsToCreate = tasksOfTaskGroup.map((task) => {
-        return {
-          taskId: task.id,
-          userId: nextResponsibleUserId,
-        };
-      });
-      await db.insert(assignmentTable).values(assignmentsToCreate);
+      await db
+        .insert(assignmentTable)
+        .values(
+          taskIds.map((taskId) => ({ taskId, userId: nextResponsibleUserId })),
+        );
     }
   }
+}
+
+async function getNextResponsibleUserId(
+  taskGroupId: number,
+  userIds: number[],
+) {
+  const currentAssignments = await db
+    .select()
+    .from(assignmentTable)
+    .innerJoin(taskTable, eq(taskTable.id, assignmentTable.taskId))
+    .innerJoin(taskGroupTable, eq(taskGroupTable.id, taskTable.taskGroupId))
+    .where(
+      sql`${assignmentTable.createdAt} >= NOW() - ${taskGroupTable.interval} AND ${taskGroupTable.id} = ${taskGroupId}`,
+    );
+
+  /* If there already are current assignments, return the userId of one of the current assignments
+   (It doesn't matter which one, they should all be assigned to the same user) */
+  if (currentAssignments.length != 0) {
+    return currentAssignments[0].assignment.userId;
+  }
+
+  const lastAssignments = await dbGetAssignmentsForTaskGroup(
+    taskGroupId,
+    userIds.length,
+  );
+
+  const userIdsWithoutAnyAssignments = userIds.filter(
+    (userId) =>
+      !lastAssignments.some(({ assignment }) => assignment.userId === userId),
+  );
+
+  let nextResponsibleUserId: number;
+  if (userIdsWithoutAnyAssignments.length === 0) {
+    nextResponsibleUserId =
+      lastAssignments[lastAssignments.length - 1].assignment.userId;
+  } else {
+    nextResponsibleUserId = randomFromArray(userIdsWithoutAnyAssignments);
+  }
+  return nextResponsibleUserId;
 }
