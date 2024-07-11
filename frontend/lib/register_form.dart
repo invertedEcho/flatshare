b752e357746f58084a250456695313240a0b7ccb
Jakob Stechow <jakob.stechow@pm.me>
2024-07-11 16:54:21 -0700

    feat: style login and register form a bit (#53)

--- frontend/lib/register_form.dart
+++ frontend/lib/register_form.dart
@@ -42,63 +42,78 @@ class RegisterFormState extends State<RegisterForm> {
     super.dispose();
   }
 
+  Future<void> _handleRegister() async {
+    if (_formKey.currentState!.validate()) {
+      await register(usernameController.text, passwordController.text,
+          emailController.text);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Processing Data')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Form(
         key: _formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-                decoration: const InputDecoration(
-                    icon: Icon(Icons.person), labelText: 'Username'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a username';
-                  }
-                  return null;
-                },
-                controller: usernameController),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: TextFormField(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              TextFormField(
                   decoration: const InputDecoration(
-                      icon: Icon(Icons.email), labelText: 'Email'),
+                      icon: Icon(Icons.person), labelText: 'Username'),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
-                      return 'Please enter an Email';
+                      return 'Please enter a username';
                     }
                     return null;
                   },
-                  controller: emailController),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: TextFormField(
-                  decoration: const InputDecoration(
-                      icon: Icon(Icons.password), labelText: 'Password'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a password';
-                    }
-                    return null;
-                  },
-                  controller: passwordController),
-            ),
-            FilledButton(
-              onPressed: () async {
-                if (_formKey.currentState!.validate()) {
-                  var res = await register(usernameController.text,
-                      passwordController.text, emailController.text);
-
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Processing Data')),
-                  );
-                }
-              },
-              child: const Text('Submit'),
-            ),
-          ],
+                  controller: usernameController),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: TextFormField(
+                    decoration: const InputDecoration(
+                        icon: Icon(Icons.email), labelText: 'Email'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter an Email';
+                      }
+                      return null;
+                    },
+                    controller: emailController),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 22.0, top: 8.0),
+                child: TextFormField(
+                    decoration: const InputDecoration(
+                        icon: Icon(Icons.password), labelText: 'Password'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      return null;
+                    },
+                    controller: passwordController),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: FilledButton(
+                  style: ButtonStyle(
+                      shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)))),
+                  onPressed: _handleRegister,
+                  child: const Padding(
+                    padding: EdgeInsets.all(14.0),
+                    child: Text('Register'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
