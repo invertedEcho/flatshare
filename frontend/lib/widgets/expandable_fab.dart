f129f30caa02a860e37c31f1896b93358266b549
Julian Wasmeier <86231458+julian-wasmeier-titanom@users.noreply.github.com>
2024-07-12 02:04:48 +0200

    feat: add expandable action button (#51)
    * feat: add expandable action button
    
    * refactor: remove for loop
    
    * feat: make button same size, adapt colors

---
+++ frontend/lib/widgets/expandable_fab.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class ExpandableFab extends StatefulWidget {
+  const ExpandableFab({
+    super.key,
+    this.initialOpen,
+    required this.children,
+  });
+
+  final bool? initialOpen;
+  final List<Widget> children;
+
+  @override
+  State<ExpandableFab> createState() => _ExpandableFabState();
+}
+
+class _ExpandableFabState extends State<ExpandableFab>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _expandAnimation;
+  bool _open = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _open = widget.initialOpen ?? false;
+    _controller = AnimationController(
+      value: _open ? 1.0 : 0.0,
+      duration: const Duration(milliseconds: 250),
+      vsync: this,
+    );
+    _expandAnimation = CurvedAnimation(
+      curve: Curves.fastOutSlowIn,
+      reverseCurve: Curves.easeOutQuad,
+      parent: _controller,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggle() {
+    setState(() {
+      _open = !_open;
+      if (_open) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        clipBehavior: Clip.none,
+        children: [
+          _buildTapToCloseFab(),
+          Positioned(
+            right: 0,
+            bottom: 56,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: widget.children
+                    .map((child) => Column(
+                          children: [
+                            _ExpandingActionButton(
+                                progress: _expandAnimation, child: child),
+                            const SizedBox(height: 12)
+                          ],
+                        ))
+                    .toList()),
+          ),
+          _buildTapToOpenFab(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTapToCloseFab() {
+    return IgnorePointer(
+      ignoring: !_open,
+      child: AnimatedContainer(
+        transformAlignment: Alignment.center,
+        duration: const Duration(milliseconds: 250),
+        curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
+        child: AnimatedOpacity(
+          opacity: !_open ? 0.0 : 1.0,
+          curve: const Interval(0.25, 1.0, curve: Curves.easeInOut),
+          duration: const Duration(milliseconds: 250),
+          child: FloatingActionButton(
+            onPressed: _toggle,
+            backgroundColor: Theme.of(context).colorScheme.secondary,
+            shape: const CircleBorder(),
+            child: Icon(Icons.close,
+                color: Theme.of(context).colorScheme.onSecondary),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTapToOpenFab() {
+    return IgnorePointer(
+      ignoring: _open,
+      child: AnimatedContainer(
+        transformAlignment: Alignment.center,
+        duration: const Duration(milliseconds: 250),
+        curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
+        child: AnimatedOpacity(
+          opacity: _open ? 0.0 : 1.0,
+          curve: const Interval(0.25, 1.0, curve: Curves.easeInOut),
+          duration: const Duration(milliseconds: 250),
+          child: FloatingActionButton(
+            onPressed: _toggle,
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            shape: const CircleBorder(),
+            child: const Icon(Icons.create),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+@immutable
+class ActionButton extends StatelessWidget {
+  const ActionButton({
+    super.key,
+    this.onPressed,
+    required this.icon,
+  });
+
+  final VoidCallback? onPressed;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Material(
+        shape: const CircleBorder(),
+        clipBehavior: Clip.antiAlias,
+        color: theme.colorScheme.inversePrimary,
+        elevation: 4,
+        child: SizedBox(
+            height: 56,
+            width: 56,
+            child: IconButton(
+              onPressed: onPressed,
+              icon: icon,
+              color: theme.colorScheme.onSurface,
+            )));
+  }
+}
+
+@immutable
+class _ExpandingActionButton extends StatelessWidget {
+  const _ExpandingActionButton({
+    required this.progress,
+    required this.child,
+  });
+
+  final Animation<double> progress;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: progress,
+      builder: (context, child) {
+        return IgnorePointer(
+          ignoring: progress.value == 0,
+          child: child,
+        );
+      },
+      child: FadeTransition(
+        opacity: progress,
+        child: child,
+      ),
+    );
+  }
+}
