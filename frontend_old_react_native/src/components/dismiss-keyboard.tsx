0c115e56a5998ce79fac4559609c5b2a1032f242
Jakob Stechow <jakob.stechow@pm.me>
2024-07-15 21:58:08 +0200

    chore: delete react native folder (#59)

--- frontend_old_react_native/src/components/dismiss-keyboard.tsx
+++
@@ -1,9 +0,0 @@
-import { Keyboard, TouchableWithoutFeedback } from "react-native";
-
-export function DismissKeyboard({ children }: { children: React.ReactNode }) {
-  return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      {children}
-    </TouchableWithoutFeedback>
-  );
-}
--- frontend_old_react_native/src/utils/assert.ts
+++
@@ -1,8 +0,0 @@
-export function getDefinedValueOrThrow<TValue>(
-  value: TValue | undefined | null,
-): TValue {
-  if (value === undefined || value === null) {
-    throw new Error('Expected value to be defined, got undefined | null.');
-  }
-  return value;
-}
