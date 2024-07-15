eb0db0078fd7bc050beb3b2f4506c962684cb485
Jakob Stechow <jakob.stechow@pm.me>
2024-07-15 21:54:11 +0200

    fix: remove unused date utils

--- frontend/lib/utils/date.dart
+++ frontend/lib/utils/date.dart
@@ -1,10 +1,4 @@
-enum IntervalType { days, weeks, months }
-
 String parseToDueDate(DateTime dueDate) {
   String diffInDays = DateTime.now().difference(dueDate).inDays.toString();
-  return "${diffInDays}d";
-}
-
-String convertInterval(IntervalType intervalType, int intervalCount) {
-  return "";
+  return "Due in: ${diffInDays}d";
 }
