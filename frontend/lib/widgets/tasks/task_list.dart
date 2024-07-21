874935ecc4f5e27d7152ea385f6e4021ff37bb50
julian-wasmeier-titanom <julian.wasmeier@titanom.com>
2024-07-21 17:09:31 +0200

    feat: make task group editable

--- frontend/lib/widgets/tasks/task_list.dart
+++ frontend/lib/widgets/tasks/task_list.dart
@@ -1,6 +1,13 @@
+import 'package:animated_custom_dropdown/custom_dropdown.dart';
+import 'package:collection/collection.dart';
 import 'package:flatshare/fetch/task.dart';
+import 'package:flatshare/fetch/task_group.dart';
 import 'package:flatshare/models/task.dart';
+import 'package:flatshare/models/task_group.dart';
+import 'package:flatshare/models/user_group.dart';
+import 'package:flatshare/providers/user.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TaskList extends StatelessWidget {
   final List<Task> tasks;
@@ -85,6 +92,9 @@ class EditTaskFormState extends State<EditTaskForm> {
   final _formKey = GlobalKey<FormState>();
   late TextEditingController titleController;
   late TextEditingController descriptionController;
+  SingleSelectController<TaskGroup?> selectTaskGroupController =
+      SingleSelectController(null);
+  List<TaskGroup> taskGroups = [];
 
   @override
   void initState() {
@@ -92,6 +102,36 @@ class EditTaskFormState extends State<EditTaskForm> {
     titleController = TextEditingController(text: widget.task.title);
     descriptionController =
         TextEditingController(text: widget.task.description);
+
+    // Initialize the selectTaskGroupController after fetching taskGroups
+    fetchTaskGroupsAndInitialize();
+  }
+
+  Future<void> fetchTaskGroupsAndInitialize() async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    UserGroup? userGroup = userProvider.userGroup;
+    final userGroupId = userGroup?.id;
+
+    if (userGroupId != null) {
+      try {
+        // Fetch task groups
+        final result = await fetchTaskGroups(userGroupId: userGroupId);
+        selectTaskGroupController = SingleSelectController<TaskGroup?>(
+          result.firstWhereOrNull(
+            (taskGroup) => taskGroup.id == widget.task.recurringTaskGroupId,
+          ),
+        );
+
+        setState(() {
+          taskGroups = result;
+        });
+
+        // Initialize the SingleSelectController after taskGroups is populated
+      } catch (error) {
+        // Handle error here (e.g., show a message)
+        print('Error fetching task groups: $error');
+      }
+    }
   }
 
   Future<void> onSubmit(Task task) async {
@@ -99,7 +139,7 @@ class EditTaskFormState extends State<EditTaskForm> {
         taskId: task.id,
         title: titleController.text,
         description: descriptionController.text,
-        taskGroupId: task.recurringTaskGroupId);
+        taskGroupId: selectTaskGroupController.value?.id);
   }
 
   @override
@@ -127,6 +167,15 @@ class EditTaskFormState extends State<EditTaskForm> {
                       labelText: "Description (optional)"),
                 ),
                 const SizedBox(height: 20),
+                CustomDropdown.search(
+                    controller: selectTaskGroupController,
+                    decoration: const CustomDropdownDecoration(
+                        listItemStyle: TextStyle(color: Colors.black),
+                        hintStyle: TextStyle(color: Colors.black)),
+                    hintText: "Select task group",
+                    items: taskGroups,
+                    onChanged: (value) {}),
+                SizedBox(height: 20),
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
