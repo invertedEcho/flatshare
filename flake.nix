ecd35ed95338c717c010d878894f0f37cf213c17
Jakob Stechow <jakob.stechow@pm.me>
2024-12-08 21:24:48 +0100

    fix: android build for nix flake

--- flake.nix
+++ flake.nix
@@ -4,29 +4,53 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs";
+
+    # gradle v7.6.3
+    nixpkgs-gradle.url = "github:NixOS/nixpkgs/68bb040a9617ec704cb453cc921f7516d5b36cae";
+    # jdk19
+    nixpkgs-jdk.url = "github:NixOS/nixpkgs/e05f1c6f3c9e9feb968d6080196bd4361be5c2c7";
   };
 
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-gradle,
+    nixpkgs-jdk,
     flake-utils,
   }:
     flake-utils.lib.eachSystem ["x86_64-linux"] (
       system: let
         pkgs = import nixpkgs {
           inherit system;
-          config.allowUnfree = true;
+          config = {
+            allowUnfree = true;
+            android_sdk.accept_license = true;
+          };
         };
+        pkgs-gradle = import nixpkgs-gradle {inherit system;};
+        pkgs-jdk = import nixpkgs-jdk {inherit system;};
+
+        androidEnv = pkgs.androidenv.override {licenseAccepted = true;};
+
+        androidComposition = androidEnv.composeAndroidPackages {
+          buildToolsVersions = ["30.0.3" "34.0.0"];
+          platformVersions = ["34"];
+          abiVersions = ["x86_64"]; # emulator related: on an ARM machine, replace "x86_64" with
+        };
+        androidSdk = androidComposition.androidsdk;
       in {
         devShell = with pkgs;
-          mkShell {
+          mkShell rec {
+            ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/34.0.0/aapt2";
             FLUTTER_ROOT = flutter;
             DART_ROOT = "${flutter}/bin/cache/dart-sdk";
             QT_QPA_PLATFORM = "wayland;xcb"; # emulator related: try using wayland, otherwise fall back to X
             buildInputs = [
+              androidSdk
               flutter
-              gradle
-              jdk21
+              pkgs-gradle.gradle_7
+              pkgs-jdk.jdk19
               protobuf
               buf
               pandoc
@@ -36,6 +60,7 @@
               glib
               pcre2
               pkg-config
+              firebase-tools
             ];
             CMAKE_PREFIX_PATH = "${pkgs.lib.makeLibraryPath [libsecret.dev gtk3.dev]}";
 
@@ -45,6 +70,7 @@
               # also probably breaks stuff when doing a release build
               # we could provide shell scripts for building release/debug for nix users that set the correct dir
               export CMAKE_INSTALL_PREFIX=$PWD/frontend/build/linux/x64/debug/bundle
+              export PATH="$PATH":"$HOME/.pub-cache/bin"
             '';
           };
       }
