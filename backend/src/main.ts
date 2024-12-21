bffa54bdadbae1f0b8f6c5aae9c4cb0d7c661b3a
Jakob Stechow <jakob.stechow@pm.me>
2024-12-21 20:39:34 +0100

    fix: initialize firebase admin app only if required

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,29 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import firebaseAdmin from 'firebase-admin';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(3000, '0.0.0.0');
-
-  const firebaseServiceAccountJsonContent =
-    process.env.FIREBASE_SERVICE_ACCOUNT_JSON_CONTENT;
-
-  // TODO: should probably do the same for all other environment variables too.
-  if (firebaseServiceAccountJsonContent === undefined) {
-    throw new Error(
-      'FIREBASE_SERVICE_ACCOUNT_JSON_CONTENT environment variable must be set.',
-    );
-  }
-
-  firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(
-      JSON.parse(firebaseServiceAccountJsonContent),
-    ),
-  });
 
   const appUrl = await app.getUrl();
   console.log(`Flatshare Backend is running on: ${appUrl}`);
--- backend/src/notifications/notification.ts
+++ backend/src/notifications/notification.ts
@@ -1,10 +1,33 @@
 import { Message, getMessaging } from 'firebase-admin/messaging';
+import firebaseAdmin from 'firebase-admin';
+
+if (firebaseAdmin.apps.length === 0) {
+  const firebaseServiceAccountJsonContent =
+    process.env.FIREBASE_SERVICE_ACCOUNT_JSON_CONTENT;
+
+  // TODO: should probably do the same for all other environment variables too.
+  if (firebaseServiceAccountJsonContent === undefined) {
+    throw new Error(
+      'FIREBASE_SERVICE_ACCOUNT_JSON_CONTENT environment variable must be set.',
+    );
+  }
+
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(
+      JSON.parse(firebaseServiceAccountJsonContent),
+    ),
+  });
+}
 
 export async function sendFirebaseMessages({
   messages,
 }: {
   messages: Message[];
 }) {
+  if (messages.length === 0) {
+    return;
+  }
+
   const messaging = getMessaging();
   const response = await messaging.sendEach(messages);
   if (response.failureCount > 0) {
